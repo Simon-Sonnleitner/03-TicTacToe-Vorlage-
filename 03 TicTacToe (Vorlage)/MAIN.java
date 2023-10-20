103b1708ec89d929fd602d5f76749d85c9685768
Simon <jim48@gmx.de>
2023-10-20 13:38:11 +0200

    bla bla bla

--- 03 TicTacToe (Vorlage)/MAIN.java
+++ 03 TicTacToe (Vorlage)/MAIN.java
@@ -3,6 +3,6 @@ public class MAIN
     public static void main(String[] args)
     {
         TICTACTOE tictactoe = new TICTACTOE();
-        tictactoe.FeldMarkieren(0,0, 1);
+
     }
 }
--- 03 TicTacToe (Vorlage)/TICTACTOE.java
+++ 03 TicTacToe (Vorlage)/TICTACTOE.java
@@ -35,37 +35,36 @@ public class TICTACTOE extends SPIEL
             }
         }
 
-        this.zeigeKoordinatensystem(true);
+        zeigeKoordinatensystem(true);
     }
     
-        //TODO: Schreibe eine Methode zum Wechseln des Spielers. Beim Aufruf der Methode soll das Attribut aktueller_spieler von 1 auf 2 wechseln und umgekehrt.
-
+    //TODO: Schreibe eine Methode zum Wechseln des Spielers. Beim Aufruf der Methode soll das Attribut aktueller_spieler von 1 auf 2 wechseln und umgekehrt.
     /**
-     * Methode SpielerWechseln sorgt dafür, dass die Spieler sich abwechsel und ändert das Attribut aktueller_spieler geeignet.
+     * Methode SpielerWechseln sorgt dafür, dass die Spieler sich abwechseln und ändert das Attribut aktueller_spieler geeignet.
      *
      */
     public void SpielerWechseln()
     {
         if (aktueller_spieler == 1)
         {
-            this.aktueller_spieler = 0;
+            this.aktueller_spieler = 2;
         }
-        else
+        else if (aktueller_spieler == 2)
         {
-            aktueller_spieler = 0;
+            aktueller_spieler = 1;
         }
     }
     
     
     
     //TODO: Schreibe eine Methode FeldMarkieren(...) zum Markieren der Felder. Wenn das mit x und y ausgewählte Feld leer ist, soll es mit der übergebenen
-    //spielerNummer besetzt werden. Zudem soll, je nach spielerNummer (1 oder 2) an passender Stelle ein X oder O gezeichner werden. Verwende hierzu die Klasse FIGUR.
+    //spielerNummer besetzt werden. Zudem soll, je nach spielerNummer (1 oder 2) an passender Stelle ein X oder O gezeichnet werden. Verwende hierzu die Klasse FIGUR.
     /**
      * Methode FeldMarkieren
      *
      * @param x Ein Parameter der die x-Koordinate der Spielmatrix beschreibt. 0 ist links, 1 ist mittig, 2 ist rechts.
      * @param y Ein Parameter der die y-Koordinate der Spielmatrix beschreibt. 0 ist oben, 1 ist mittig, 2 ist unten.
-     * @param spielerNummer Ein Parameter der die Spielernummer übergibt, der gerade am Zug ist (1 oder 2).
+     * @param spielerNummer Ein Parameter, der die Spielernummer übergibt, der gerade am Zug ist (1 oder 2).
      */
     public void FeldMarkieren(int x, int y, int spielerNummer)
     {
@@ -89,23 +88,55 @@ public class TICTACTOE extends SPIEL
     /**
      * Methode klickReagieren wird stets ausgelöst, wenn man mit der Maus in das Fenster klickt.
      *
-     * @param x Ein Parameter der die x-Koorinate der Mausposition beim Klicken in Bildschirmmetern übergibt.
-     * @param y Ein Parameter der die y-Koorinate der Mausposition beim Klicken in Bildschirmmetern übergibt.
+     * @param x Ein Parameter der die x-Koordinate der Mausposition beim Klicken in Bildschirmmetern übergibt.
+     * @param y Ein Parameter der die y-Koordinate der Mausposition beim Klicken in Bildschirmmetern übergibt.
      */
     @Override
-    public void klickReagieren( double x , double y ) 
+    public void klickReagieren( double x , double y )
     {
         // erst rechts mitte, oben, unten, dann mitte mitte, oben, unten, dann links mitte, oben, unten
-        if(x >= 2.5 && y < 2.5 && y > -2.5)
+        if(x >= 2.5 && y < 2.5 && y > -2.5 && spielfeld[2][1] == 0)
         {
-            FeldMarkieren(5,0,this.aktueller_spieler);
-        } else if (x >= 2.5 && y >= 2.5) {
-            FeldMarkieren(5,5,this.aktueller_spieler);
-        } else if (x >= 2.5 && y <= -2.5) {
+            FeldMarkieren(5,0, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[2][1] = this.aktueller_spieler;
+        } else if (x >= 2.5 && y >= 2.5 && spielfeld[2][0] == 0) {
+            FeldMarkieren(5,5, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[2][0] = this.aktueller_spieler;
+        } else if (x >= 2.5 && y <= -2.5 && spielfeld[2][2] == 0) {
             FeldMarkieren(5,-5, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[2][2] = this.aktueller_spieler;
+        } else if (x >= -2.5 && x < 2.5 && y < 2.5 && y > -2.5 && spielfeld[1][1] == 0) {
+            FeldMarkieren(0,0, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[1][1] = this.aktueller_spieler;
+        } else if (x >= -2.5 && x < 2.5 && y >= 2.5 && spielfeld[1][0] == 0) {
+            FeldMarkieren(0,5, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[1][0] = this.aktueller_spieler;
+        } else if (x >= -2.5 && x < 2.5 && y <= -2.5 && spielfeld[1][2] == 0) {
+            FeldMarkieren(0,-5, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[1][2] = this.aktueller_spieler;
+        } else if (x < -2.5 && y < 2.5 && y > -2.5 && spielfeld[0][1] == 0) {
+            FeldMarkieren(-5,0, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[0][1] = this.aktueller_spieler;
+        } else if (x < -2.5 && y >= 2.5 && spielfeld[0][0] == 0) {
+            FeldMarkieren(-5, 5, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[0][0] = this.aktueller_spieler;
+        } else if (x < -2.5 && y <= -2.5 && spielfeld[0][2] == 0) {
+            FeldMarkieren(-5, -5, this.aktueller_spieler);
+            SpielerWechseln();
+            spielfeld[0][2] = this.aktueller_spieler;
         }
+
+
     }
-    
+
 
 
     
